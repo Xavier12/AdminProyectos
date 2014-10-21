02747f90711a21cbd64ecfdfe9efdaece0012e95
Xavier12 <xavier_gemi@hotmail.com>
2014-10-21 18:26:11 -0500

    add(images, js)/modified(index,style) file for project A.R

--- Docs/Proyect/index.php
+++ Docs/Proyect/index.php
@@ -2,9 +2,40 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Despacho</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
+	<script type="text/javascript" src="js/transition.js"></script>
 </head>
 <body>
+	<div id="A-contenedor" class="fireSans18">
+		<label>¡Bienvenido!</label>
+		<div id="AA-contenedor">
+			<div id="B-Formulario" class="fireSans15">
+				<form method="POST">
+					<label for="txtCorreo">Email</label>
+					<input type="text" name="txtCorreo" id="txtCorreo">
+
+					<label for="txtPass">Password</label>
+					<input type="password" name="txtPass" id="txtPass">
+
+					<input type="submit" value="Login" name="btEnviar">
+					
+				</form>
+			</div>
+		</div>
+	</div>
+
+	 <section id="C-Contenido" class="col-lg-12 col-md-12 col-xs-12" >
+          <div id="diaporama" >
+              <img id="1" src="images/D1.jpg" >
+              <img id="2" src="images/D2.jpg" >
+              <img id="3" src="images/D3.jpg" >
+
+          </div>
+      </section>
+
+
 	
 </body>
 </html>
---
+++ Docs/Proyect/js/transition.js
@@ -0,0 +1,20 @@
+var indice=2;
+$(document).ready(function(){
+	var images=$('#diaporama img');
+	images.hide();
+	$('#diaporama img:first').fadeIn(2000);
+	transicion();
+});
+
+function transicion()
+{
+	setInterval(function(){
+		var num=$('#diaporama > img').size();
+
+		if (indice>num) {indice=1;}
+		$('#diaporama > img').fadeOut(1900);
+		$('#diaporama > img#'+indice).fadeIn(2000);
+		indice=indice+1;
+	},6000)
+
+}
