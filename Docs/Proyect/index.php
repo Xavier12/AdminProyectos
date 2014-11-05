cbf2fd9d142b2a69e7ca159f70bc3433a4f24d86
Xavier12 <xavier_gemi@hotmail.com>
2014-11-04 19:05:38 -0600

    add files/update

--- Docs/Proyect/index.php
+++ Docs/Proyect/index.php
@@ -1,39 +1,87 @@
 <?php 
 require('class/db.class.php');
 require('class/conf.class.php');
+require('Secretaria/class/Clientes.php');
 
 $db=Db::getInstance();
 
 
-$user = isset($_POST['txtUser']) && !empty($_POST['txtUser']) ? $_POST['txtUser'] : "";
-$pass= isset($_POST['txtPass'])  && !empty($_POST['txtPass']) ? $_POST['txtPass'] : "";
+// $user = isset($_POST['txtUser']) && !empty($_POST['txtUser']) ? $_POST['txtUser'] : "";
+// $pass = isset($_POST['txtPass']) && !empty($_POST['txtPass']) ? $_POST['txtPass'] : "";
 
-$user=$db->replace($user);
-$pass=$db->replace($pass);
+// $user=$db->replace($user);
+// $pass=$db->replace($pass);
+
+// echo $pass;
+// $resul=Cliente::getAllUser($db);
+// echo $resul;
+// while($row=$db->obtener_fila($resul,0))
+// {
+// 	$var=$row['Nombre'];
+// 	echo $var;
+// 	echo "ppe";
+// }
 
 
-$userSecre=$db->ejecutar("SELECT Usuario,Password FROM usuriosecre WHERE Usuario='$user' AND Password='$pass'");
-$secre=$db->num_rows($userSecre);
 
-$userClient=$db->ejecutar("SELECT Usuario,Password FROM usuariocliente WHERE Usuario='$user' AND Password='$pass'");
-$client=$db->num_rows($userClient);
 
-$userLawyer=$db->ejecutar("SELECT Usuario,Password FROM usuarioabogado WHERE Usuario='$user' AND Password='$pass'");
-$lawyer=$db->num_rows($userLawyer);
 
-if($secre>0)
-{
-	echo "<script>alert('Bienvenido(a) Secretaria'); window.location='Secretaria/index.php'; </script>";
-}elseif ($client>0)
-{
-	echo "<script>alert('Bienvenido(a) Cliente'); window.location='Cliente/index.php'; </script>";
-}elseif ($lawyer>0)
-{
-	echo "<script>alert('Bienvenido(a) Abogado');</script>";
-}elseif ($client==0 || $lawyer==0 || $secre==0) 
-{
-	echo "<script>alert('Usuario o contraseña incorrectos');  </script>";
-}
+// $userClient=$db->ejecutar("SELECT Usuario,Password FROM usuariocliente WHERE Usuario='$user' AND Password='$pass'");
+// $client=$db->num_rows($userClient);
+
+// $userLawyer=$db->ejecutar("SELECT Usuario,Password FROM usuarioabogado WHERE Usuario='$user' AND Password='$pass'");
+// $lawyer=$db->num_rows($userLawyer);
+
+// if($secre>0)
+// {
+// 	echo "<script>alert('Bienvenido(a) Secretaria'); window.location='Secretaria/index.php'; </script>";
+// }elseif ($client>0)
+// {
+// 	echo "<script>alert('Bienvenido(a) Cliente'); window.location='Cliente/index.php'; </script>";
+// }elseif ($lawyer>0)
+// {
+// 	echo "<script>alert('Bienvenido(a) Abogado');</script>";
+// }elseif ($client==0 || $lawyer==0 || $secre==0) 
+// {
+// 	echo "<script>alert('Usuario o contraseña incorrectos');  </script>";
+// }
 require('Views/Cabeza.php');
+
+if(isset($_POST['btLog']))
+{
+	$user = isset($_POST['txtUser']) && !empty($_POST['txtUser']) ? $_POST['txtUser'] : "";
+	$pass = isset($_POST['txtPass']) && !empty($_POST['txtPass']) ? $_POST['txtPass'] : "";
+
+	$user=$db->replace($user);
+	$pass=$db->replace($pass);
+
+	$userSecre=$db->ejecutar("SELECT Usuario,Password FROM usuriosecre WHERE Usuario='$user' AND Password='$pass'");
+	$secre=$db->num_rows($userSecre,0);
+
+	$userClient=$db->ejecutar("SELECT Usuario,Password FROM usuariocliente WHERE Usuario='$user' AND Password='$pass'");
+	$client=$db->num_rows($userClient,0);
+
+	$userLawyer=$db->ejecutar("SELECT Usuario,Password FROM usuarioabogado WHERE Usuario='$user' AND Password='$pass'");
+	$lawyer=$db->num_rows($userLawyer,0);
+
+
+	if($secre>0)
+	{
+		echo "<script>alert('Bienvenido(a) Secretaria'); window.location='Secretaria/index.php';</script>";
+	}
+	else 
+	if($client>0){
+		echo "<script>alert('Bienvenido cliente'); window.location='Cliente/index.php';</script>";
+	}
+	else 
+	if ($lawyer>0)
+	{
+		echo "<script>alert('Bienvenido abogado(a)'); window.location='Abogado/index.php';</script>";
+	}
+	else{echo "<script>alert('Error en Usuario/Contraseña');</script>";}
+}
 require('Views/index.php');
+
+
+
 ?>
